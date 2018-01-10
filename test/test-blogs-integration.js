55bc751de4654d4b514a7b30acaea5a69a2e5c9f
David Park <dysp92@gmail.com>
2018-01-10 09:48:36 -0800

    added functions

--- test/test-blogs-integration.js
+++ test/test-blogs-integration.js
@@ -10,11 +10,44 @@ const should = chai.should;
 const {BlogPost} = require('../models');
 const {runServer, app, closeServer} = require('../server');
 const {TEST_DATABASE_URL} = require('../config');
-const {seedData} = require('../seed-data');
 
 chai.use(chaiHttp);
 
 //Seeding data goes here
+
+
+
+function seedBlogPostData() {
+  const seedData = [];
+
+  for (let i=1; i<=10; i++) {
+    seedData.push(generateBlogPostData());
+
+  return BlogPost.insertMany(seedData);
+}
+
+function generateTitle() {
+  const titles = ['Title #1', 'Title #2', 'Title #3', 'Title #4', 'Title #5'];
+  return titles[Math.floor(Math.random() * titles.length)];
+}
+
+function generateContent() {
+  const contents = ['Content #1', 'Content #2', 'Content #3', 'Content #4', 'Content #5'];
+  return contents[Math.floor(Math.random() * contents.length)];
+}
+
+function generateAuthor() {
+  const authors = ['Author #1', 'Author #2', 'Author #3'];
+  return authors[Math.floor(Math.random() * authors.length)];
+}
+
+function generateBlogPostData() {
+  return {
+    title: generateTitle(),
+    content: generateContent(),
+    author: generateAuthor()
+  }
+}
 
 function tearDownDb() {
   console.warn('Deleting database');
@@ -23,13 +56,12 @@ function tearDownDb() {
 
 describe('Blog Posts API resource', function() {
 
-
   before(function() {
     return runServer(TEST_DATABASE_URL);
   });
 
   beforeEach(function() {
-    return BlogPost.insertMany(seedData);
+    return seedData;
   });
 
   afterEach(function() {
@@ -42,7 +74,22 @@ describe('Blog Posts API resource', function() {
 
   //Get
   describe('GET endpoint', function() {
-
+    it('should return all blog posts', function() {
+      let res;
+      console.log(seedData);
+      return chai.request(app)
+        .get('/posts')
+        .then(function(_res) {
+          console.log(_res);
+          res = _res;
+          res.should.have.status(200);
+          (res.body.posts).should.have.length.of.at.least(1);
+          return seedData.count();
+        })
+        .then(function(count) {
+          (res.body.posts).should.have.length.of(count);
+        });
+    });
   });
 
   //Post
